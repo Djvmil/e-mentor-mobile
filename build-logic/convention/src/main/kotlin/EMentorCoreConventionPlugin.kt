a5590e41b0d43530cc2f6ba5af0787c26ac938a0
moustaphas.dieme <ndieme16@hotmail.com>
2024-04-27 03:37:34 +0200

    solution temporaire pour fixe le bug en commentant les impl de kotlin(test)

--- build-logic/convention/src/main/kotlin/EMentorCoreConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/EMentorCoreConventionPlugin.kt
@@ -1,11 +1,8 @@
-import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.kotlin
 import org.gradle.kotlin.dsl.project
 
 class EMentorCoreConventionPlugin : Plugin<Project> {
@@ -16,7 +13,7 @@ class EMentorCoreConventionPlugin : Plugin<Project> {
             }
             /*extensions.configure<LibraryExtension> {
                 defaultConfig {
-                    testInstrumentationRunner = "com.google.samples.apps.nowinandroid.core.testing.NiaTestRunner"
+                    testInstrumentationRunner = "com.djvmil.entretienmentor.core.testing.EMTestRunner"
                 }
             }*/
 
